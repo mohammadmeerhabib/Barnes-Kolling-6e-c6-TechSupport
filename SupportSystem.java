74e5d0ea103ed62cec0de40bde49011026a138b5
Mohammadmeerhabib <meerhabib@gmail.com>
2023-12-23 13:57:23 +0000

    further refinement of Support System

--- SupportSystem.java
+++ SupportSystem.java
@@ -1,61 +1,61 @@
-
 /**
- * Write a description of class SupportSystem here.
- *
- * @author (your name)
- * @version (a version number or a date)
+ * reads via reader and outputs response via responder
  */
 public class SupportSystem
 {
-    // instance variables - replace the example below with your own
-    InputReader reader;    
-    Responder responder;
+    private Responder responder;
+    private InputReader reader;
     /**
-     * Constructor for objects of class SupportSystem
-     */
+     * constructs responders as an instance of class Responder and 
+     * reader as an instance of class InputReader
+     */    
     public SupportSystem()
     {
-        // initialise instance variables
         reader = new InputReader();
         responder = new Responder();
-        
     }
-
     /**
-     * start to run support system, takes input via input reader and generates response 
-     * via Responder
+     * start method starts the process and takes input via input reader and
+     * generates a response via responder.
      * @param none
      * @return none
      */
     public void start()
     {
-        boolean finished = false;
         printWelcome();
-        while (!finished){
+        boolean flag = false;
+        while (!flag) {
             String input = reader.getInput().trim().toLowerCase();
-            if (input.startsWith("bye")){
-                finished = true;
-                printGoodbye();
+            if (input.startsWith("bye")) {
+                flag = true;
             } else {
-            System.out.println(responder.generateResponse());
+                System.out.println(responder.generateResponse());
             }
         }
+        printGoodbye();
     }
-    /**
-     * prints welcome message
+    
+    /*
+     * printWelcome prints welcome message
      * @param none
      * @return none
      */
     public void printWelcome()
     {
-        System.out.println("Welcome to Technical Support System." );
+        System.out.println("Hi, Welcome to Tech Support System site!");
         System.out.println();
-        System.out.println("Please tell us about your problem.");
-        System.out.println("We will assist you with any problem you might have.");
-        System.out.println("Please type 'bye' to exit our system.");
+        System.out.println("Please enter issue you are facing clearly");
+        System.out.println("This will help us give you advice propertly");
+        System.out.println("When done, please enter 'bye', to log out-");
     }
+    
+    /**
+     * print goodBye prints goodbye message.
+     * @param none
+     * @return none
+     */
     public void printGoodbye()
     {
-        System.out.println("Nice talking to you. Bye...");
+        System.out.println("Bye! Nice talking to you-");
     }
 }
