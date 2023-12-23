71496130c8b36b23df9e7c618a90f313acbc1b75
Mohammadmeerhabib <meerhabib@gmail.com>
2023-12-23 17:07:47 +0000

    responder further developed

--- Responder.java
+++ Responder.java
@@ -1,4 +1,5 @@
-
+import java.util.ArrayList;
+import java.util.Random;
 /**
  * Write a description of class Responder here.
  *
@@ -8,14 +9,16 @@
 public class Responder
 {
     // instance variables - replace the example below with your own
-    
-
+    public Random randomGenerator;
+    public ArrayList<String> responses;
     /**
      * Constructor for objects of class Responder
      */
     public Responder()
     {
-        
+        randomGenerator = new Random();
+        responses = new ArrayList<>();
+        listResponses();
     }
 
     /**
@@ -26,7 +29,16 @@ public class Responder
      */
     public String generateResponse()
     {
-        // put your code here
-        return "That sounds interesting. Tell me more...";
+        return responses.get(randomGenerator.nextInt(responses.size()));
+    }
+    public void listResponses()
+    {
+        responses.add("That sounds odd. Could you explain a bit more please?");
+        responses.add("No other customer had this issue. What system do you have?");
+        responses.add("I need more detailed information");
+        responses.add("This parts is covered in the manual. Can you please read that" + 
+        " and see if it helps.");
+        responses.add("Your description is not clear. Anyone more experience with you?");
+        responses.add("Can you please elaborate on it.");
     }
 }
